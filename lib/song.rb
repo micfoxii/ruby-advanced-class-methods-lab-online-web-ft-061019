db546232a6f11ea4553ee81d3ffeced4f8dd00e5
Michael Fox <michael.c.fox.ii@gmail.com>
2019-06-17 18:18:41 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -43,8 +43,8 @@ class Song
   
   def self.new_from_filename(name)
     song = self.new 
-    song.name = name.split((" - ")[1])
-    song.artist_name = name.split((" - ")[0])
+    song.name = (name.split(" - ")[1])
+    song.artist_name = (name.split(" - ")[0])
     song
     end
 end
